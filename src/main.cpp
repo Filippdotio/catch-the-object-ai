3fe69e0039a0159ca37d3705dad6e32b9bc3c638
Filipp Schnotalla <Filipp Schnotalla>
2025-12-07 06:55:37 +0100

    Project structure setup

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({800, 600u}), "Catch The Object");
     window.setFramerateLimit(144);
 
     while (window.isOpen())
